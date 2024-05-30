6ab1f77a7c4a4990f2e49894494e3c2854f28bfe
DEYVISON PAULA <deyvison.paula@atos.net>
2024-05-30 19:23:31 -0300

    Props Post / Iterando jsx

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -6,6 +6,36 @@ import styles from './App.module.css';
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/38982851?v=4',
+      name: 'Deyvison J. Paula',
+      role: 'Desenvolvedor Full Stack | .NET + React'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galerinha 💢'},
+      { type: 'paragraph', content: 'Componentes servem para não repetir código e componentes podem ser utilizados em partes menores com códigos reutilizaveis'},
+      { type: 'link', content: 'deyvison.full/whiteForce' }
+    ],
+    publishedAt: new Date('2024-05-28 20:00:00'),
+  },
+  {
+  id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galerinha 💢'},
+      { type: 'paragraph', content: 'Componentes servem para não repetir código e componentes podem ser utilizados em partes menores com códigos reutilizaveis'},
+      { type: 'link', content: 'deyvison.full/whiteForce' }
+    ],
+    publishedAt: new Date('2024-05-30 07:55:00'),
+  }
+];
 
 function App() {
 
@@ -18,7 +48,15 @@ function App() {
         <Sidebar />
 
         <main>
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
 
       </div>
--- 01-fundamentos-reactjs/src/components/Post/index.jsx
+++ 01-fundamentos-reactjs/src/components/Post/index.jsx
@@ -1,35 +1,59 @@
 /* eslint-disable react/prop-types */
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from '../Avatar';
 import { Comment } from '../Comment';
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post({ author, content, publishedAt  }) {
+    // const publishedDateFormatted = new Intl.DateTimeFormat('pt-BR', {
+    //     day: '2-digit',
+    //     month: 'long',
+    //     hour: '2-digit',
+    //     minute: '2-digit'
+    // }).format(publishedAt);
+    const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' H:mm'h'", {
+        locale: ptBR
+    });
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    });
+
     return (
         <article className={styles.post}>
             <header>
                 
                 <div className={styles.author}>
                     <Avatar
-                        srcAvatar="https://avatars.githubusercontent.com/u/38982851?v=4" 
+                        srcAvatar={author.avatarUrl}
                     />
                     <div className={styles.authorInfo}>
-                        <strong>Deyvison Paula</strong>
-                        <span>Full Stack</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title="25/05/2024 15:18h" dateTime="2024-05-25 15:18:30">Publicado há 20min</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galerinha 💢</p>
-                <p>Vamos que vamos, Ultrapasse seus limites ... Plus Ultra! </p>
-                <p>Componentes servem para não repetir código e componentes podem ser utilizados em partes menores com códigos reutilizaveis</p>
-                <p>💥{' '}<a href="">deyvison.full/whiteForce </a></p>
-                <p>
-                    <a href="">#AceleraSenior </a>{' '}
-                    <a href="">#Rocketseat </a>{' '}
-                </p>
+                {content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return 
+                        <p>
+                            {line.content}
+                        </p>;
+                    } else if (line.type === 'link') {
+                        return 
+                        <p>
+                            <a href="#">{line.content}</a>
+                        </p>
+                    }
+                })}    
+                
             </div>
 
             <form className={styles.commentForm}>
