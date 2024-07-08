4b9a42bbbe612e2bf2ade2a682e1247077a7e38d
DEYVISON PAULA <deyvison.paula@atos.net>
2024-07-08 11:26:21 -0300

    Estilos globais

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,6 +1,19 @@
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+import { Button } from './components/Button';
+import { GlobalStyle } from './styles/global';
+
 export function App() {
 
   return (
-    <div>Olá Mundo!</div>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary"/>
+      <Button variant="primary"/>
+      <Button variant="primary"/>
+      <Button variant="primary"/>
+      <Button variant="primary"/>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
